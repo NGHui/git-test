84d59c0f52095c817ab14f8b8d4820b8a6a9c18a
聂广辉 <2284129306@qq.com>
2019-11-05 10:20:28 +0800

    使用IDEA-git 提交

---
+++ IEDEA-Git-Test/src/com/hui/git/HelloGit.java
@@ -0,0 +1,19 @@
+package com.hui.git;
+
+/**
+ * @author 辉
+ * 座右铭:坚持总能遇见更好的自己!
+ * @date 2019/11/5
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+
+}
