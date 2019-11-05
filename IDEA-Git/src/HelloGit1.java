8f41b5ab35687d4383fd1400ce46a583e67e45cc
聂广辉 <2284129306@qq.com>
2019-11-05 11:09:11 +0800

    分支提交

---
+++ IDEA-Git/src/HelloGit1.java
@@ -0,0 +1,14 @@
+/**
+ * @author 辉
+ * 座右铭:坚持总能遇见更好的自己!
+ * @date 2019/11/5
+ */
+public class HelloGit1 {
+    public static void main(String[] args) {
+        System.out.println("SSSSSSSSSS");
+        System.out.println("SSSSSSSSSS");
+        System.out.println("SSSSSSSSSS");
+        System.out.println("SSSSSSSSSS");
+        System.out.println("SSSSSSSSSS");
+    }
+}
